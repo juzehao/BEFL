bcac881b275bb711f981310878ec795b772cfb27
juzehao <1246337892@qq.com>
2024-09-17 23:14:59 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# BEFL
+## Description
+<strong>BEFL</strong>, a joint optimization framework aimed at balancing three objectives: enhancing global model accuracy, minimizing total energy consumption, and reducing energy usage disparities among devices. First, taking into account the communication constraints of MEIoT and the heterogeneity of devices, we employed the Sequential Least Squares Programming (SLSQP) algorithm for the rational allocation of communication resources. Based on this, we introduce a heuristic client selection algorithm that combines cluster partitioning with utility-driven approaches to alleviate both the total energy consumption of all devices and the discrepancies in energy usage.Furthermore, we utilize the proposed heuristic client selection algorithm as a template for offline imitation learning during pre-training, while adopting a ranking-based reinforcement learning approach online to further boost training efficiency. 
+<div style="text-align: center;">
+    <img src="./overview.png" alt="风景图片" width="700"/>
+</div>
 
+## How to run the Code
+run <strong>main.ipynb</strong>
--- serverFL/BEFL_offline.py
+++ serverFL/BEFL_offline.py
@@ -1,47 +1,51 @@
 import copy
+import timeit
+import clientFL.Client as Client
+from tqdm import tqdm
 import numpy as np
 import random
 import torch
 from torch.utils.data import DataLoader
-from sklearn.metrics import f1_score, recall_score, precision_score
+from sklearn.decomposition import PCA
+import torch.nn as nn
+import RL.DQL as DQL
+import numpy as np
+from sklearn.metrics import f1_score,recall_score,precision_score
 from torch.nn import functional as F
-import clientFL.Client as Client
-
-class Server_TIFL(object):
-
-    def __init__(self, num_clients, global_model, dict_clients, loss_fct, B, dataset_test, learning_rate, momentum, clients_info, tier_thresholds,bandwidth):
+from scipy.optimize import minimize
+global energy
+class BEFL_offline(object):
+    
+    def __init__(self, num_clients, global_model, dict_clients, loss_fct, B, dataset_test, learning_rate, momentum, clients_info,bandwidth,alpha):
         """
-        初始化Server_TIFL对象。
+        Initialize the Server_FLASHRL object.
 
-        参数:
-        - num_clients: 客户端的数量。
-        - global_model: 全局模型。
-        - dict_clients: 包含每个客户端信息的字典。
-        - loss_fct: 训练使用的损失函数。
-        - B: 批处理大小。
-        - dataset_test: 用于评估的测试数据集。
-        - learning_rate: 训练的学习率。
-        - momentum: 训练的动量参数。
-        - clients_info: 客户端的信息列表。
-        - tier_thresholds: 用于分类客户端的门槛值列表。
+        Parameters:
+        - num_clients: The number of clients in the system.
+        - global_model: The global model.
+        - dict_clients: A dictionary containing information about each client.
+        - loss_fct: The loss function used for training.
+        - B: The size of the batch.
+        - dataset_test: The test dataset used for evaluation.
+        - learning_rate: The learning rate for training.
+        - momentum: The momentum parameter for training.
+        - clients_info: Information about the clients for simulation purposes.
         """
         self.N = num_clients
-        self.model = global_model
-        self.list_clients = []
-        self.B = B
         self.bandwidth=bandwidth
+        self.model = global_model 
+        self.list_clients = [] 
+        self.B = B
+        self.alpha=alpha
         self.dataset_test = dataset_test
-        self.testdataloader = DataLoader(self.dataset_test, batch_size=self.B)
+        self.testdataloader = DataLoader(self.dataset_test, batch_size= self.B)
         self.dict_clients = dict_clients
         self.loss_function = copy.deepcopy(loss_fct)
         self.number_parameters = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
         self.clients_info = clients_info
         self.create_clients(learning_rate, momentum)
-        
-        # 初始化基于门槛值的层级分类
-        self.tier_thresholds = tier_thresholds
-        self.client_tiers = self.categorize_clients_into_tiers()
 
+   
     def create_clients(self, learning_rate, momentum):
         cpt = 0
         for client_name in self.dict_clients.keys():
@@ -63,8 +67,11 @@ class Server_TIFL(object):
                 self.clients_info[cpt][8],
 
             )
+            print(client.get_size())
             self.list_clients.append(client)
             cpt += 1
+            print()
+
 
     def weight_scalling_factor(self, client, active_clients):
         '''
@@ -143,71 +150,32 @@ class Server_TIFL(object):
         weight_flatten = [item for sublist in weight_flatten for item in sublist]
 
         return weight_flatten
-    
-    def categorize_clients_into_tiers(self):
-        """
-        根据客户端的性能将其分类到不同的层级中。
 
-        返回:
-        - 客户端层级的字典，其中包含客户端的下标。
-        """
-        client_tiers = {tier: [] for tier in range(len(self.tier_thresholds) + 1)}
+    def flatten_state(self, state_list):
+        '''
+        Flatten a list of states.
 
-        for index, client in enumerate(self.list_clients):
-            performance = self.evaluate_client_performance(client)
-            tier = self.get_client_tier(performance)
-            client_tiers[tier].append(index)  # 将客户端的下标添加到层级中
+        Parameters:
+        - state_list: List of states where each state is represented as a list of sublists.
 
-        return client_tiers
+        Returns:
+        - Flattened list of states as a torch.Tensor.
+        '''
+        result_list = []
+        max_length = max(len(sublist) for sublist in state_list)  # Find the maximum length of sublists
 
-    def evaluate_client_performance(self, client):
-        return client.CalculationTrainTime() + client.CalculationTranslationTime(self.bandwidth/10)
-
-    def get_client_tier(self, performance):
-        for i, threshold in enumerate(self.tier_thresholds):
-            if performance <= threshold:
-                return i
-        return len(self.tier_thresholds)
-
-    def select_tier(self):
-        """
-        基于预定义的概率或自适应方法选择一个层级。
-
-        返回:
-        - 选择的层级。
-        """
-        tier_probs = [1 / len(self.client_tiers) for _ in range(len(self.client_tiers))]
-        selected_tier = np.random.choice(list(self.client_tiers.keys()), p=tier_probs)
-        return selected_tier
-
-    def select_clients_from_tier(self, tier, C):
-        """
-        从特定层级中选择客户端的下标。
-
-        参数:
-        - tier: 要从中选择客户端的层级。
-        - C: 选择的客户端比例。
-
-        返回:
-        - 选择的客户端下标列表。
-        """
-        clients_in_tier = self.client_tiers[tier]
-        while not clients_in_tier:  # 如果层级为空，则重新选择层级
-            tier = self.select_tier()
-            clients_in_tier = self.client_tiers[tier]
+        for i in range(max_length):
+            for sublist in state_list:
+                if i < len(sublist):
+                    element = sublist[i]
+                    if isinstance(element, list):
+                        result_list.extend(element)
+                    else:
+                        result_list.append(element)
         
-        m = int(max(C * len(clients_in_tier), 1))
-
-        if m > len(clients_in_tier):
-            selected_clients = clients_in_tier
-        else:
-            selected_clients = random.sample(clients_in_tier, m)
+        return torch.Tensor(result_list)
         
-        return selected_clients
-
-
-
-    def calculate_energy_consumption(self,active_clients_index):
+    def calculate_energy_consumption(self,active_clients_index,bandwidth_list):
         """
         计算各个客户端设备的能耗
 
@@ -220,13 +188,49 @@ class Server_TIFL(object):
         for index in range(0,len(self.list_clients)):
             # 获取客户端的训练数据量、频率、带宽等信息
             total_energy=0
-            if index in active_clients_index:
-                total_energy =  self.list_clients[index].CalculationTrainEngry()+self.list_clients[index].CalculationTranslationEngry(self.bandwidth/10)
-            
             # 将计算结果存入列表
             energy_consumption.append(total_energy)
+        i=0
+        for index in active_clients_index:
+            total_energy =  self.list_clients[index].CalculationTrainEngry()+self.list_clients[index].CalculationTranslationEngry(self.bandwidth*bandwidth_list[i])
+            i+=1
+            energy_consumption[index]=total_energy
         return energy_consumption  # 列表长度与客户端数量相同
 
+
+    def calculate_value_function(self, client, global_weights):
+        """
+        Calculate the value function for a client based on the loss of its data.
+        
+        Parameters:
+        - client: The client for which to calculate the value function.
+        - global_weights: The current global model weights.
+        
+        Returns:
+        - The calculated value function.
+        """
+        # Set the client's model to the current global weights
+        client.set_weights(global_weights)
+        # Calculate the loss on the client's data
+        loss = client.calculate_loss()
+        # Normalize the loss based on the number of samples
+        value = loss / np.sqrt(client.get_size())
+        return value
+
+
+    def choosefromclient(self,client_index,number):
+        choose=[]
+        for index in client_index:
+            choose.append(self.list_clients[index].EnergyFunction(self.bandwidth/10))
+        answer_index=np.argsort(choose)[-number:]
+        answer=[]
+        for index in answer_index:
+            answer.append(client_index[index])
+        return answer
+
+            
+
+    
     def global_train(self, comms_round, C, E, mu, lamb, rep_init, batch_size, verbose_test=1, verbos=0, init_weights=None, init_commround=None, reputation_init=None):
         m = int(max(C*self.N, 1))
         total_energy_consumption = np.zeros(len(self.list_clients))
@@ -240,7 +244,9 @@ class Server_TIFL(object):
         best_accuracy = 0
         num_param = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
         state_list = []
-
+        high_client=[]
+        low_client=[]
+        
         if init_weights is not None:
             reputation_clients_t = reputation_init
             reputation_list.append(copy.deepcopy(reputation_clients_t))
@@ -252,34 +258,75 @@ class Server_TIFL(object):
             reputation_clients_t = np.full(self.N, rep_init)
             reputation_list.append(copy.deepcopy(reputation_clients_t))
             comm_start = 0
-
+            client_energy=[]
+            for index in range(len(self.list_clients)):
+                client_energy.append(self.list_clients[index].CalculationTranslationEngry(self.bandwidth))
+            low_client=np.argsort(client_energy)[:(int(len(self.list_clients)*self.alpha))]
+            high_client=np.argsort(client_energy)[(int(len(self.list_clients)*self.alpha)):]
+        
         for comm_round in range(comm_start, comms_round):
             rounds.append(comm_round+1)
             global_weights = self.model.state_dict()
-            active_clients_index = self.select_clients_from_tier(comm_round, C)
-            print(active_clients_index)
+            active_clients_index_low=self.choosefromclient(low_client,int(10*self.alpha))
+            active_clients_index_high=self.choosefromclient(high_client,10-int(10*self.alpha))
+            active_clients_index = active_clients_index_low+active_clients_index_high
             active_clients_index.sort()
+
+            bandwidth_list = []
+            num = 10
+            for i in range(0, num):
+                bandwidth_list.append(1.0 / num)
+            roundengry = 0
+
+            energy = []
+            for index in active_clients_index:
+                energy.append(self.list_clients[index].CalculationTranslationEngry(self.bandwidth))
+
+            def fun(x):
+                return sum(e / x_i for e, x_i in zip(energy, x))
+
+            e = 1e-10
+
+            cons1 = (
+                {'type': 'eq', 'fun': fun},  # 约束条件：xyz=1
+                *[
+                    {'type': 'ineq', 'fun': lambda x, i=i: x[i] - e}
+                    for i in range(len(energy))
+                ],
+                *[
+                    {'type': 'ineq', 'fun': lambda x, i=i: 1 - x[i]}
+                    for i in range(len(energy))
+                ],
+                {'type': 'ineq', 'fun': lambda x: 1 - sum(x)}  # 新增约束条件：x[i] 的和小于等于 1
+            )
+
+            res = minimize(fun, bandwidth_list, method='SLSQP', constraints=cons1)
+            bandwidth_list = res.x
+
             scaled_local_weight_list = []
             active_clients = [self.list_clients[i] for i in active_clients_index]
             t=[]
+            index=0
             for client_index in active_clients_index:
+                self.list_clients[client_index].count+=1
                 client_w = self.list_clients[client_index].train(global_weights, E, mu, verbos)
                 client_scaling_factor = self.weight_scalling_factor(self.list_clients[client_index], active_clients)
                 client_scaled_weight = self.scale_model_weights(client_w, client_scaling_factor)
                 scaled_local_weight_list.append(client_scaled_weight)
-                t.append(self.list_clients[client_index].CalculationTranslationTime(self.bandwidth/10)+self.list_clients[client_index].CalculationTrainTime())
+                t.append(self.list_clients[client_index].CalculationTranslationTime(bandwidth_list[index]*self.bandwidth)+self.list_clients[client_index].CalculationTrainTime())
+                index+=1
             average_weights = self.sum_scaled_weights(scaled_local_weight_list)
             self.model.load_state_dict(average_weights)
             Accuray_global_t, loss_test = self.test()
             if verbose_test == 1:
-                print("Training round n :", (comm_round+1),", Test accuarcy : ", round(Accuray_global_t*100, 2) ,", Test loss :", round(loss_test, 2))
+                print("Training round n :", (comm_round+1),", Test accuarcy : ", round(Accuray_global_t.numpy()*100, 2) ,", Test loss :", round(loss_test, 2))
             if Accuray_global_t > best_accuracy:
                 best_accuracy = Accuray_global_t
                 best_model_weights = copy.deepcopy(average_weights)
             accuarcy.append(Accuray_global_t)
             loss.append(loss_test)
             time_rounds.append(time_rounds[-1]+max(t))
-            round_energy_consumption = self.calculate_energy_consumption(active_clients_index)  
+            round_energy_consumption = self.calculate_energy_consumption(active_clients_index,bandwidth_list)  
             total_energy_consumption += np.array(round_energy_consumption)
             comm_round+=1
 
@@ -294,28 +341,35 @@ class Server_TIFL(object):
         }
 
         return dict_result
-
+    
+    
     def test(self):
         """
-        在测试数据集上评估全局模型。
+        Evaluate the global model with the test dataset for a task other than fall detection.
 
-        返回:
-        - 测试数据集上的准确率和损失。
+        Returns:
+        - The accuracy and loss on the test dataset.
         """
+        # Set the model to evaluation mode
         self.model.eval()
-        total, correct = 0, 0
-        loss_final_test = 0
-        y_true = []
-        y_pred = []
-        for data in self.testdataloader:
-            inputs, labels = data
-            outputs = self.model(inputs)
-            loss_final_test += F.cross_entropy(outputs, labels)
-            predicted = torch.argmax(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-            y_true.extend(labels.numpy())
-            y_pred.extend(predicted.numpy())
-        accuracy = correct / total
-        loss_final_test = loss_final_test / len(self.testdataloader)
-        return accuracy, loss_final_test.item()
+
+        # Testing
+        test_loss = 0
+        correct = 0
+
+        # Iterate through the test dataset
+        with torch.no_grad():
+            for idx, (data, target) in enumerate(self.testdataloader):
+                log_probs = self.model(data)
+                # Sum up batch loss
+                test_loss += torch.nn.functional.cross_entropy(log_probs, target, reduction='sum').item()
+
+                # Get the index of the max log-probability
+                y_pred = log_probs.data.max(1, keepdim=True)[1]
+                correct += y_pred.eq(target.data.view_as(y_pred)).long().cpu().sum()
+
+            # Calculate average test loss and accuracy
+            test_loss /= len(self.testdataloader.dataset)
+            accuracy = correct / len(self.testdataloader.dataset)
+
+        return accuracy, test_loss
